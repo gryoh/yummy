71c8a094a16940a9f7bfafe64321d576866b61d2
오형준 <hungry6459@gmail.com>
2023-08-22 21:51:16 +0900

    서버별 환경설정 수정(로컬/개발/운영)

--- configs/config.production.ts
+++ configs/config.production.ts
@@ -2,7 +2,7 @@
 import getConfigs from "./config.common";
 
 // 환경마다 달라져야 할 변수, 값들을 정의합니다. (여기는 local 환경에 맞는 값을 지정합니다.)
-const baseUrl = 'http://localhost:3000';
+const baseUrl = 'http://gryoh.synology.me:8083';
 const mode = 'production';
 
 // 환경마다 달라져야 할 값들을 getConfig 함수에 전달합니다.
--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
+//todo 서버별로 변수 정의해서 destination 앞에 넣기
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  devtool: isDevelopment ? 'hidden-source-map' : 'inline-source-map',
+  reactStrictMode: true,
+  swcMinify: false, //minifying해주는 기능, 나중에 완전히 개발 완료되면 true로 바꿀 예정
+  async rewrites() {
+    return [
+      {
+        source: "/v1/:path*",
+        destination: isDevelopment ? "http://localhost:8080/v1/:path*" : "http://gryoh.synology.me:8083/v1/:path*",
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
